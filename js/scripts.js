ec68010f34e4c348acffafef76001db9e24f3cb5
Joan Nekoye <joannekoye@gmail.com>
2020-03-04 16:55:10 +0300

    make html and css

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+function myFunction() {
+  // Get the snackbar DIV
+  var x = document.getElementById("submit-vol");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+function myFunction2() {
+  // Get the snackbar DIV
+  var x = document.getElementById("sign-up");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+function myFunction3() {
+  // Get the snackbar DIV
+  var x = document.getElementById("mpesa");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
